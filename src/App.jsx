21aa9e62d0039201f60536d3acd7db2a43e0a520
Alperen İlgaz <alperenilgaz3@gmail.com>
2024-05-22 02:10:16 +0300

    delete : codePen links & buttons

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ const TeamData = [
     teamPlayers:[
       {
         id:1,
-        name:"Alperen İlgaz",
+        name:"Alperen İlgaz -",
       },
       {
         id:2,
@@ -26,7 +26,7 @@ const TeamData = [
     teamPlayers:[
       {
         id:1,
-        name:"Alperen İlgaz",
+        name:"Alperen İlgaz -",
       },
       {
         id:2,
@@ -43,7 +43,7 @@ const TeamData = [
     teamPlayers:[
       {
         id:1,
-        name:"Alperen İlgaz",
+        name:"Alperen İlgaz -",
       },
       {
         id:2,
@@ -71,43 +71,12 @@ function TeamCard({ place, icon, team, teamPlayers, score }) {
        ))}
       </div>
 
-      <div className="total-score">
+ 
+      </div>
+        <div className="total-score">
         <p>Puan:{score}</p>
       </div>
-      
-      </div>
-      
-      <div className="codePen">
-        <button className='btn'><i class="bi bi-braces"></i>CodePen</button>
-        <div className="modal">
-                <div className="header">
-                <i class="bi bi-dash-lg"></i>
-                <i class="bi bi-fullscreen-exit"></i>
-                <i class="bi bi-x-lg"></i>
-                </div>
-                <div className="content-wrap">
-                <div className="row">
-                  <ul>
-                    <li>1</li>
-                    <li>2</li>
-                    <li>3</li>
-                    <li>4</li>
-                    <li>5</li>
-                  </ul>
-                </div>
-                <div className="content">
-                <ul>
-                    <li style={{color:"#58aaed"}}>meta name=<span style={{color:"orange"}}>"viewport"</span>content=<span style={{color:"orange"}}>"width=device-width, initial-scale=1"</span></li>
-                    <li style={{color:"#58aaed"}}>link rel=<span style={{color:"orange"}}>"apple-touch-icon"</span> href=<span style={{color:"orange"}}>"%PUBLIC_URL%/logo192.png"</span></li>
-                    <li style={{color:"#58aaed"}}>link rel=<span style={{color:"orange"}}>"manifest"</span> href=<span style={{color:"orange"}}>"%PUBLIC_URL%/manifest.json"</span></li>
-                    <li style={{color:"#58aaed"}}>meta charset=<span style={{color:"orange"}}>"utf-8"</span> </li>
-                    <li style={{color:"#58aaed"}}>link rel=<span style={{color:"orange"}}>"icon"</span> href=<span style={{color:"orange"}}>"%PUBLIC_URL%/favicon.ico"</span> </li>
-                  </ul>
-                </div>
-              </div>
-          </div>
-  
-        </div>
+     
           
      
        
